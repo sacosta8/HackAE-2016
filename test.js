131ab53ae5faa69e4fc71e48403c6665a3d494d5
aorella1 <aorella1@binghamton.edu>
2016-11-12 15:42:51 -0500

    Added POST and DELETE

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var addLocationController = require('./controllers/addLocationController')
 var app = express();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('assets'));
+app.use(express.static('./public'));
 
+addLocationController(app);
+/*
 app.get('/map',function(req, res){
   res.render('map');
 });
@@ -24,4 +27,6 @@ app.get('/profile/:name', function(req,res){
   var data = {age: 29, job: 'janitor'};
   res.render('profile',{person: req.params.name, data: data});
 });
+*/
 app.listen(3000);
+console.log('listening 3000');
