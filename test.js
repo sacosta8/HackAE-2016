c7313fc2cba514c43ead3ef72c2092dd41b7db52
aorella1 <aorella1@binghamton.edu>
2016-11-12 09:49:38 -0500

    app.post added

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
-app.get('/',function(req, res){
+app.get('/map',function(req, res){
+  res.render('map');
+});
+
+app.post('/map', urlencodedParser, function(req, res){
+  console.log(req.body);;
   res.render('map');
 });
 
