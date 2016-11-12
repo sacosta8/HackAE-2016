a578a7117f5cf2c1378382278e60cede0feb4327
aorella1 <aorella1@binghamton.edu>
2016-11-12 09:48:41 -0500

    Added POST form to map page

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
 var express = require('express');
-
 var app = express();
 
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
 app.get('/',function(req, res){
-  res.sendFile(__dirname + '/public/index.html')
+  res.render('map');
 });
 
 app.get('/contact',function(req, res){
   res.send('this is the contact page')
 });
 
-app.get('/profile/:id', function(req,res){
-  res.send('You requested ' + req.params.id);
+app.get('/profile/:name', function(req,res){
+  var data = {age: 29, job: 'janitor'};
+  res.render('profile',{person: req.params.name, data: data});
 });
 app.listen(3000);
