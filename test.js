e7e419830d029e9a832940252b120a956b1325c9
RadOctocode <happylife345@gmail.com>
2017-01-12 16:33:21 -0500

    added expression validator static webpage no longer being used

--- test.js
+++ test.js
@@ -1,35 +1,23 @@
 var express = require('express');
+var expressValidator = require('express-validator');
 var bodyParser = require('body-parser');
-//var addLocationController = require('./controllers/addLocationController')
-var app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
+var app = express();
+app.use(expressValidator());
+var urlencodedParser = bodyParser.urlencoded({ extended: true});
+
+// My DB config
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost:27017/locations');
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
-//addLocationController(app);
-app.get('/map',function(req, res){
-  res.render('map');
-});
-/*
+//home page
 app.get('/map',function(req, res){
   res.render('map');
 });
 
-app.post('/map', urlencodedParser, function(req, res){
-  console.log(req.body);;
-  res.render('map');
-});
-
-app.get('/contact',function(req, res){
-  res.send('this is the contact page')
-});
-
-app.get('/profile/:name', function(req,res){
-  var data = {age: 29, job: 'janitor'};
-  res.render('profile',{person: req.params.name, data: data});
-});
-*/
 app.listen(3000);
 console.log('listening 3000');
